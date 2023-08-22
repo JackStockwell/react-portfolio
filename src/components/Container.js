f8e0abe058c430642a04186959c8112bdbae6a4b
Jack Stockwell <95311730+JackStockwell@users.noreply.github.com>
2023-08-22 20:20:09 +0100

    continued work on container.js

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,25 +1,33 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
-
+import Home from "./pages/Home";
 
 export default function Container() {
 
-    let pageData = {
-        page: 'Home'
+
+
+    const [ page, setPage ] = useState('Home')
+
+    const renderPage = () => {
+        switch (page) {
+            case 'Home':
+                return <Home />
+            case 'Projects':
+                return <Projects />
+            default:
+                break;
+        }
     }
 
-    const [ data, setData ] = useState(pageData)
-
     const handlePageChange = (e) => {
-        let { name, value } = e.target
-
-        console.log(e.target)
-
+        // Sets the page to the parsed value from the event.
+        setPage(e.target.text)
     }
 
     return (
         <main>
             <Navbar handlePageChange={handlePageChange}/>
+            {renderPage()}
         </main>
     )
 }
