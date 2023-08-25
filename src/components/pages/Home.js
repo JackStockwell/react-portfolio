7561f0405b3cf4cb6c0f4244ecdb5cc376b6b69b
JackStockwell <stockwell.jack@gmail.com>
2023-08-25 23:13:23 +0100

    css on navbar

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -17,36 +17,40 @@ export default function Home() {
                     <div className='max-120'>
                         <Reveal>
                             <h3>About me</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+                                velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+                                ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+                                non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
+                                ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
+                                rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
+                                tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
+                                porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
+                                vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+                                fames ac ante ipsum primis in faucibus.
+                            </p>
                         </Reveal>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-                            velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-                            ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-                            non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-                            ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-                            rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-                            tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-                            porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-                            vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-                            fames ac ante ipsum primis in faucibus.
-                        </p>
                     </div>
                 </section>
                 <section className='section'>
-                    <div>
-                        <h3>Skills</h3>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-                            velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-                            ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-                            non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-                            ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-                            rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-                            tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-                            porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-                            vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-                            fames ac ante ipsum primis in faucibus. 
-                        </p>
+                    <div className='max-120'>
+                        <Reveal>
+                            <h3>Skills</h3>
+                            <div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+                                    velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+                                    ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+                                    non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
+                                    ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
+                                    rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
+                                    tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
+                                    porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
+                                    vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+                                    fames ac ante ipsum primis in faucibus. 
+                                </p>                                
+                            </div>
+                        </Reveal>
                     </div>
                 </section>
             </div>
