a96a17e3389a53c19a43b23e19b8ec93a52e0b9c
JackStockwell <stockwell.jack@gmail.com>
2023-08-27 12:16:51 +0100

    changed color schema, worked on projects css

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -12,27 +12,53 @@ export default function Project({ page }) {
     return (
         <>
             <Header page={page} header={headingData}></Header>
-            <section className='section'>
-                <div className='max-120'>
-                    <Reveal>
-                        <section className='grid'>
-
+            <div className='content-wrap'>
+                <section className='section'>
+                    <div className='max-120'>
+                        <Reveal>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+                                velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+                                ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
+                                non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
+                                ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
+                                rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
+                                tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
+                                porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
+                                vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
+                                fames ac ante ipsum primis in faucibus.
+                            </p>
+                        </Reveal>
+                    </div>
+                </section>
+                <section className='section b-inline'>
+                    <div className='max-120'>
+                        <section className='projects'>
+                            <section>
+                                <div>
+                                    <h3>Project 1</h3>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+                                        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+                                        ac dui fermentum, sed luctus urna tincidunt.
+                                    </p> 
+                                </div>
+                                <img className='p-img' src='https://placehold.co/600x400' />
+                            </section>
+                            <section>
+                                <div>
+                                    <p>                                  
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
+                                        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
+                                        ac dui fermentum, sed luctus urna tincidunt.
+                                    </p>
+                                    <h3>Project 2</h3>    
+                                </div>                            
+                            </section>
                         </section>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-                            velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-                            ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-                            non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-                            ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-                            rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-                            tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-                            porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-                            vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-                            fames ac ante ipsum primis in faucibus.
-                        </p>
-                    </Reveal>
-                </div>
-            </section>
+                    </div>
+                </section>
+            </div>
         </>
     )
 }
